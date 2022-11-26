48ada68c16187f80ec23831136c657c229e28aa8
Ben Binder <ben.binder11@gmail.com>
2022-11-26 17:29:41 -0600

    Implement 'api/categories' POST, PUT, and DELETE routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -26,23 +26,62 @@ router.get('/:id', async (req, res) => {
     if (category_data) {
       res.status(200).json(category_data);
     } else {
-      res.status(400).json({'err':'No category with the given ID was found!'});
+      res.status(404).json({message : 'No category with the given ID was found!'});
     }
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    /* req.body should look like this...
+    {
+      category_name: "XXXX",
+    }
+    */
+    const category_data = await Category.create(req.body);
+    res.status(201).json(category_data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const category_data = await Category.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+    if (category_data) {
+      res.status(200).json(category_data);
+    } else {
+      res.status(404).json({message: 'No category with the given ID was found!'});
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const category_data = await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (category_data) {
+      res.status(200).json(category_data);
+    } else {
+      res.status(404).json({message: 'No category with the given ID was found!' });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;

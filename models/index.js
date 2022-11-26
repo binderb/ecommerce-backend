00febf59355c34720ba8e0cb16b727edd3cf2d7f
Ben Binder <ben.binder11@gmail.com>
2022-11-26 16:51:57 -0600

    Implement sync, models, associations, and 'api/categories/' GET routes.

--- models/index.js
+++ models/index.js
@@ -5,12 +5,29 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  uniqueKey: 'unique_id',
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  uniqueKey: 'unique_id',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,34 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const category_data = await Category.findAll({
+      include: [{model : Product}]
+    });
+    res.status(200).json(category_data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category_data = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    if (category_data) {
+      res.status(200).json(category_data);
+    } else {
+      res.status(400).json({'err':'No category with the given ID was found!'});
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
